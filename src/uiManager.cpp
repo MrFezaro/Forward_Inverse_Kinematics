45356cae6ab3f5f223c427ec07a31b09d5b12f65
MrFezaro <116104181+MrFezaro@users.noreply.github.com>
2024-11-27 10:51:12 +0100

    added easter egg

--- src/uiManager.cpp
+++ src/uiManager.cpp
@@ -29,6 +29,7 @@ void uiManager::handleKinematics() {
         isForwardKinematics = !isForwardKinematics;
         paramsChanged = true;
     }
+
     if (isForwardKinematics) {
         float angle1 = kinematicChainInstance.getJointAngles()[0] * (180.0f / M_PI);
         float angle2 = kinematicChainInstance.getJointAngles()[1] * (180.0f / M_PI);
@@ -38,37 +39,47 @@ void uiManager::handleKinematics() {
             kinematicChainInstance.setJointAngles(0, angle1);
             paramsChanged = true;
         }
+
         if (ImGui::SliderFloat("Angle Joint 2", &angle2, 0.0f, 360.0f)) {
             kinematicChainInstance.setJointAngles(1, angle2);
             paramsChanged = true;
         }
+
         if (ImGui::SliderFloat("Angle Joint 3", &angle3, 0.0f, 360.0f)) {
             kinematicChainInstance.setJointAngles(2, angle3);
             paramsChanged = true;
         }
+
+        if (std::abs(angle1 - 69.0f) < 0.5f || std::abs(angle2 - 69.0f) < 0.5f || std::abs(angle3 - 69.0f) < 0.5f) {
+            ImGui::TextColored(ImVec4(1.0, 0.84, 0.0, 1.0), "Nice");
+        }
+
         auto [x, y] = kinematicChainInstance.forwardKinematics();
         kinematicChainInstance.setTarget({x, y});
         ImGui::Text("End Effector Position:");
         ImGui::Text("X: %.2f", x);
         ImGui::Text("Y: %.2f", y);
+
     } else {
         point target = kinematicChainInstance.getTarget();
         if (ImGui::SliderFloat("Target X", &target.x, -10.0f, 10.0f)) {
             kinematicChainInstance.setTarget(target);
             paramsChanged = true;
         }
+
         if (ImGui::SliderFloat("Target Y", &target.y, -10.0f, 10.0f)) {
             kinematicChainInstance.setTarget(target);
             paramsChanged = true;
         }
+
+        const std::vector<float> &jointAngles = kinematicChainInstance.getJointAngles();
+        ImGui::Text("Calculated Angles:");
+        ImGui::Text("Angle Joint 1: %.2f", jointAngles[0] * (180.0f / M_PI));
+        ImGui::Text("Angle Joint 2: %.2f", jointAngles[1] * (180.0f / M_PI));
+        ImGui::Text("Angle Joint 3: %.2f", jointAngles[2] * (180.0f / M_PI));
+
         if (!kinematicChainInstance.inverseKinematicsCCD()) {
             ImGui::TextColored(ImVec4(1, 0, 0, 1), "Target is out of bounds!");
-        } else {
-            const std::vector<float> &jointAngles = kinematicChainInstance.getJointAngles();
-            ImGui::Text("Calculated Angles:");
-            ImGui::Text("Angle Joint 1: %.2f", jointAngles[0] * (180.0f / M_PI));
-            ImGui::Text("Angle Joint 2: %.2f", jointAngles[1] * (180.0f / M_PI));
-            ImGui::Text("Angle Joint 3: %.2f", jointAngles[2] * (180.0f / M_PI));
         }
     }
 }
